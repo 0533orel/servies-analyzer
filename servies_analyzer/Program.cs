eddea33dfa7174f5eb19a7b18893a634eabceb96
orel <0533orel@gmail.com>
2025-05-12 17:49:46 +0300

    get number

--- servies_analyzer/Program.cs
+++ servies_analyzer/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+
+namespace servies_analyser
+{
+    class program()
+    {
+        static void Main(string[] args)
+        {
+
+
+            //  1
+            List<double> getNumbers()
+            {
+                string userInput = inputUser();
+                List<double> numsList = getlist(userInput);
+                return numsList;
+            }
+            
+            
+
+            
+            string inputUser()
+            {
+                string userInput;
+                while (true)
+                {
+                    Console.WriteLine("Enter a series of numbers with a comma between each number.");
+                    userInput = Console.ReadLine();
+                    if (isNull(userInput))
+                    {
+                        Console.WriteLine("invalid input. blease try again.");
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                } 
+                return userInput;
+            }
+
+            
+            bool isNull(string strInput)
+            {
+                if (string.IsNullOrEmpty(strInput))
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            
+
+            List<double> getlist(string strInput)
+            {
+                string[] strArray = strInput.Split(",");
+
+                List<double> numsList = new List<double>();
+
+                for (int i = 0; i < strArray.Length; i++)
+                {
+                    numsList.Add(double.Parse(strArray[i]));
+                }
+                return numsList;
+            }
+
+
+            
+        }
+    }
+}
